b37ea32c16320db0f0112b7ba2f7aaf0ba40e12d
Robert Metzger <rmetzger@apache.org>
2016-04-20 17:59:54 +0200

    reworked flink scripts

--- bigtop-packages/src/common/flink/install_flink.sh
+++ bigtop-packages/src/common/flink/install_flink.sh
@@ -22,13 +22,13 @@ usage() {
 usage: $0 <options>
   Required not-so-options:
      --build-dir=DIR             path to flink dist.dir
+     --source-dir=DIR            path to package shared files dir
      --prefix=PREFIX             path to install into
+
   Optional options:
-     --doc-dir=DIR               path to install docs into [/usr/share/doc/flink]
      --lib-dir=DIR               path to install flink home [/usr/lib/flink]
      --installed-lib-dir=DIR     path where lib-dir will end up on target system
      --bin-dir=DIR               path to install bins [/usr/bin]
-     --examples-dir=DIR          path to install examples [doc-dir/examples]
      ... [ see source for more similar options ]
   "
   exit 1
@@ -38,11 +38,10 @@ OPTS=$(getopt \
   -n $0 \
   -o '' \
   -l 'prefix:' \
-  -l 'doc-dir:' \
   -l 'lib-dir:' \
   -l 'installed-lib-dir:' \
   -l 'bin-dir:' \
-  -l 'examples-dir:' \
+  -l 'source-dir:' \
   -l 'build-dir:' -- "$@")
 
 if [ $? != 0 ] ; then
@@ -58,8 +57,8 @@ while true ; do
         --build-dir)
         BUILD_DIR=$2 ; shift 2
         ;;
-        --doc-dir)
-        DOC_DIR=$2 ; shift 2
+        --source-dir)
+        SOURCE_DIR=$2 ; shift 2
         ;;
         --lib-dir)
         LIB_DIR=$2 ; shift 2
@@ -69,9 +68,6 @@ while true ; do
         ;;
         --bin-dir)
         BIN_DIR=$2 ; shift 2
-        ;;
-        --examples-dir)
-        EXAMPLES_DIR=$2 ; shift 2
         ;;
         --)
         shift ; break
@@ -84,55 +80,55 @@ while true ; do
     esac
 done
 
-for var in PREFIX BUILD_DIR ; do
+for var in PREFIX BUILD_DIR SOURCE_DIR ; do
   if [ -z "$(eval "echo \$$var")" ]; then
     echo Missing param: $var
     usage
   fi
 done
 
-#MAN_DIR=${MAN_DIR:-/usr/share/man/man1}
+# load bigtop component versions
+if [ -f "$SOURCE_DIR/bigtop.bom" ]; then
+  . $SOURCE_DIR/bigtop.bom
+fi
+
 
 LIB_DIR=${LIB_DIR:-/usr/lib/flink}
 INSTALLED_LIB_DIR=${INSTALLED_LIB_DIR:-/usr/lib/flink}
 BIN_DIR=${BIN_DIR:-/usr/bin}
 CONF_DIR=${CONF_DIR:-/etc/flink/conf.dist}
-EXAMPLES_DIR=${EXAMPLES_DIR:-$DOC_DIR/examples}
-DOC_DIR=${DOC_DIR:-/usr/share/doc/flink}
 
 install -d -m 0755 $PREFIX/$LIB_DIR
 install -d -m 0755 $PREFIX/$LIB_DIR/bin
 install -d -m 0755 $PREFIX/$LIB_DIR/lib
-install -d -m 0755 $PREFIX/$DOC_DIR
+install -d -m 0755 $PREFIX/$LIB_DIR/examples
+install -d -m 0755 $PREFIX/$LIB_DIR/resources
+install -d -m 0755 $PREFIX/$CONF_DIR
 install -d -m 0755 $PREFIX/var/lib/flink
 install -d -m 0755 $PREFIX/var/log/flink
 install -d -m 0755 $PREFIX/var/run/flink
 
 cp -ra ${BUILD_DIR}/lib/* $PREFIX/${LIB_DIR}/lib/
-cp -a ${BUILD_DIR}/*.txt $PREFIX/$DOC_DIR
-cp -a ${BUILD_DIR}/LICENSE $PREFIX/$DOC_DIR
-cp -a ${BUILD_DIR}/NOTICE $PREFIX/$DOC_DIR
-cp -a ${BUILD_DIR}/bin/* $PREFIX/${LIB_DIR}/bin
+cp -a ${BUILD_DIR}/bin/* $PREFIX/${LIB_DIR}/bin/
+# delete Windows start scripts
 rm -rf $PREFIX/${LIB_DIR}/bin/*.cmd
 
-# Copy in the configuration files
-install -d -m 0755 $PREFIX/$CONF_DIR
+# Copy the configuration files
 cp -a ${BUILD_DIR}/conf/* $PREFIX/$CONF_DIR
 ln -s /etc/flink/conf $PREFIX/$LIB_DIR/conf
 
-# Copy in the examples,tools,log and resources
-cp -a ${BUILD_DIR}/examples/ $PREFIX/$DOC_DIR/
-cp -ra ${BUILD_DIR}/log $PREFIX/${LIB_DIR}
-cp -ra ${BUILD_DIR}/resources $PREFIX/${LIB_DIR}
-cp -ra ${BUILD_DIR}/tools $PREFIX/${LIB_DIR}
+cp -ra ${BUILD_DIR}/examples/* $PREFIX/${LIB_DIR}/examples/
+cp -ra ${BUILD_DIR}/resources/* $PREFIX/${LIB_DIR}/resources/
 
+cp ${BUILD_DIR}/{LICENSE,NOTICE,README.txt} ${PREFIX}/${LIB_DIR}/
 
 # Copy in the /usr/bin/flink wrapper
 install -d -m 0755 $PREFIX/$BIN_DIR
 cat > $PREFIX/$BIN_DIR/flink <<EOF
 #!/bin/bash
+
 # Autodetect JAVA_HOME if not defined
-/usr/lib/bigtop-utils/bigtop-detect-javahome
+. /usr/lib/bigtop-utils/bigtop-detect-javahome
 
 export HADOOP_HOME=${HADOOP_HOME:-/usr/lib/hadoop}
 export HADOOP_CONF_DIR=${HADOOP_CONF_DIR:-/etc/hadoop/conf}
@@ -142,5 +138,4 @@ export FLINK_CONF_DIR=${FLINK_CONF_DIR:-$CONF_DIR}
 exec $INSTALLED_LIB_DIR/bin/flink "\$@"
 EOF
 chmod 755 $PREFIX/$BIN_DIR/flink
-#chmod 755 $PREFIX/$LIB_DIR/log/*
-#chmod 755 $PREFIX/$CONF_DIR/*
+
